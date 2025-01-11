8668dabcc85ee5b2339c4c71b9ee647b62890a2b
Anish Rathod <1anishrathod@gmail.com>
2025-01-11 08:31:22 -0500

    Finished vision code for alignment.

--- src/main/java/frc/robot/commands/AlignCenterCommand.java
+++ src/main/java/frc/robot/commands/AlignCenterCommand.java
@@ -1,8 +1,55 @@
 package frc.robot.commands;
 
-import edu.wpi.first.math.controller.PIDController;
-import frc.utils.devices.PhotonVision;
+import java.util.function.BooleanSupplier;
 
-public class AlignCenterCommand {
-    
+import frc.robot.subsystems.Drivetrain;
+import frc.utils.devices.PhotonVision;
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.wpilibj2.command.Command;
+
+import frc.robot.Constants.DriveConstants.DriveModes;
+
+import frc.robot.Constants.AutoConstants;
+
+
+public class AlignCenterCommand extends Command{
+   private Drivetrain driveSubsystem;
+   PIDController pid = new PIDController(AutoConstants.kPXController,AutoConstants.kPYController,AutoConstants.kPThetaController);
+
+   Double commandRot;
+   BooleanSupplier isFieldRelative;
+   BooleanSupplier isRateLimited;
+   
+
+
+   public AlignCenterCommand(Double rotationSpeed,BooleanSupplier fieldRelative, BooleanSupplier rateLimit, Drivetrain subsystem){
+        commandRot = rotationSpeed;
+        rotationSpeed = pid.calculate(PhotonVision.centerYaw,0);
+        isFieldRelative = fieldRelative;
+        isRateLimited = rateLimit;
+
+        driveSubsystem = subsystem;
+
+        addRequirements(driveSubsystem);
+   } 
+   @Override
+   public void initialize() {
+       // Set the drive mode
+       driveSubsystem.setDriveMode(DriveModes.alignLeftReef);
+   }
+
+   @Override
+   public void execute() {
+       // include the actual rotation lock into this
+       driveSubsystem.drive(0, 0, commandRot, isFieldRelative.getAsBoolean(), isRateLimited.getAsBoolean());
+   }
+
+   @Override
+   public void end(boolean interrupted) {
+   }
+
+   @Override
+   public boolean isFinished() {
+       return (driveSubsystem.getDriveMode() != DriveModes.alignLeftReef || Math.abs(commandRot) < 0.04);
+   }
 }
--- src/main/java/frc/utils/devices/PhotonVision.java
+++ src/main/java/frc/utils/devices/PhotonVision.java
@@ -5,34 +5,34 @@ import java.util.List;
 import org.photonvision.PhotonCamera;
 import org.photonvision.targeting.PhotonPipelineResult;
 import org.photonvision.targeting.PhotonTrackedTarget;
-import edu.wpi.first.math.controller.PIDController;
+
 
 
 public class PhotonVision{
-    PhotonCamera cameraRight = new PhotonCamera("Right");
+    static PhotonCamera cameraRight = new PhotonCamera("Right");
     static PhotonCamera cameraLeft = new PhotonCamera("Left");
-        PhotonCamera cameraCenter = new PhotonCamera("Center");
-        
-        PhotonPipelineResult rightResult = cameraRight.getLatestResult();
-         static PhotonPipelineResult leftResult = cameraLeft.getLatestResult();
-         PhotonPipelineResult centerResult = cameraCenter.getLatestResult();
-         
-         boolean rightHasTargets = rightResult.hasTargets();
-         boolean leftHasTargets = leftResult.hasTargets();
-         boolean centerHasTargets = centerResult.hasTargets();
-         
-             // Get a list of currently tracked targets.
-         List<PhotonTrackedTarget> rightTargets = rightResult.getTargets();
-         List<PhotonTrackedTarget> leftTargets = leftResult.getTargets();
-         List<PhotonTrackedTarget> centerTargets = centerResult.getTargets();
-         
-         PhotonTrackedTarget closestRightTarget = rightResult.getBestTarget();
-         static PhotonTrackedTarget closestLeftTarget = leftResult.getBestTarget();
-        PhotonTrackedTarget closestCenterTarget = centerResult.getBestTarget();
-        
-        double rightYaw = closestRightTarget.getYaw();
-        public static double leftYaw = closestLeftTarget.getYaw();
-
+    static PhotonCamera cameraCenter = new PhotonCamera("Center");
+            
+    static PhotonPipelineResult rightResult = cameraRight.getLatestResult();
+    static PhotonPipelineResult leftResult = cameraLeft.getLatestResult();
+    static PhotonPipelineResult centerResult = cameraCenter.getLatestResult();
+                 
+    boolean rightHasTargets = rightResult.hasTargets();
+    boolean leftHasTargets = leftResult.hasTargets();
+    boolean centerHasTargets = centerResult.hasTargets();
+                 
+    // Get a list of currently tracked targets.
+    List<PhotonTrackedTarget> rightTargets = rightResult.getTargets();
+    List<PhotonTrackedTarget> leftTargets = leftResult.getTargets();
+    List<PhotonTrackedTarget> centerTargets = centerResult.getTargets();
+                 
+    static PhotonTrackedTarget closestRightTarget = rightResult.getBestTarget();
+    static PhotonTrackedTarget closestLeftTarget = leftResult.getBestTarget();
+    static PhotonTrackedTarget closestCenterTarget = centerResult.getBestTarget();
+                 
+    public static double rightYaw = closestRightTarget.getYaw();
+    public static double leftYaw = closestLeftTarget.getYaw();
+    public static double centerYaw = closestCenterTarget.getYaw();
     
     
 
