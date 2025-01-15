7c83a596d3433c04b3208ec3d464d6a3d7ac2000
Anish Rathod <1anishrathod@gmail.com>
2025-01-15 10:36:38 -0500

    MAXMotion Rev stuff is really easy.

--- src/main/java/frc/robot/subsystems/Elevator.java
+++ src/main/java/frc/robot/subsystems/Elevator.java
@@ -11,11 +11,13 @@ import com.revrobotics.spark.config.ClosedLoopConfig.FeedbackSensor;
 import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 import com.revrobotics.spark.config.SparkMaxConfig;
 import com.revrobotics.spark.config.ClosedLoopConfig;
+import com.revrobotics.spark.SparkClosedLoopController;
 
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 import frc.robot.Constants.ElevatorConstants;
+
 
 // import com.revrobotics.spark.config.SparkMaxConfig.follow;
 
@@ -28,38 +30,45 @@ public class Elevator extends SubsystemBase{
     private final SparkMaxConfig kLeftConfig = new SparkMaxConfig();
     private final SparkMaxConfig kRightConfig = new SparkMaxConfig();
 
-    private final RelativeEncoder kLeftEncoder;
+    // private final RelativeEncoder kLeftEncoder;
+
+    private final SparkClosedLoopController kLeftController;
+
 
     public Elevator(){
         this.kLeftMotor = new SparkMax(ElevatorConstants.kLeftElevatorMotorCanId, MotorType.kBrushless);
         this.kRightMotor = new SparkMax(ElevatorConstants.kRightElevatorMotorCanId, MotorType.kBrushless);
         
 
-        this.kLeftEncoder = kLeftMotor.getEncoder();
+        //this.kLeftEncoder = kLeftMotor.getEncoder();
+        kRightConfig.follow(kLeftMotor, true);
 
-        kLeftConfig.inverted(true);
-        kRightConfig.inverted(false);
+        kLeftConfig.inverted(false);
 
         kLeftConfig.idleMode(IdleMode.kBrake);
-        kLeftConfig.idleMode(IdleMode.kBrake);
+        kRightConfig.idleMode(IdleMode.kBrake);
+
+        kLeftController = kLeftMotor.getClosedLoopController();
 
         kLeftConfig.closedLoop.feedbackSensor(FeedbackSensor.kPrimaryEncoder).pid(
             Constants.ElevatorConstants.elevatorP,
             Constants.ElevatorConstants.elevatorI,
             Constants.ElevatorConstants.elevatorD);
+
         kRightConfig.closedLoop.feedbackSensor(FeedbackSensor.kPrimaryEncoder).pid(Constants.ElevatorConstants.elevatorP,
             Constants.ElevatorConstants.elevatorI,
             Constants.ElevatorConstants.elevatorD);
 
         this.kLeftMotor.configure(kLeftConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
         this.kRightMotor.configure(kRightConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
-
-
-       
-        
-
-        
     }
+
+    private void setLeftMotorSpeed(double speed, double pos) {
+        double desiredSpeed = speed;
+        // kLeftController.calculate(kLeftEncoder.getPosition(), pos);
+        kLeftController.setReference(pos, SparkBase.ControlType.kMAXMotionPositionControl);
+    }
+
  
 }
 
