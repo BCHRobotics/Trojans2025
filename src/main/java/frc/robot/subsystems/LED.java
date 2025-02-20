5666c4c89c6f2958586386f54b6a8db01e41b05f
Anish Rathod <1anishrathod@gmail.com>
2025-02-20 13:42:02 -0500

    new LED subsystem

---
+++ src/main/java/frc/robot/subsystems/LED.java
@@ -0,0 +1,18 @@
+package frc.robot.subsystems;
+import edu.wpi.first.wpilibj.motorcontrol.Spark;
+
+public class LED {
+    Spark blinkin; 
+
+    int PWMPort;
+
+    public LED(int PWMPort) {
+        this.PWMPort = PWMPort;
+        blinkin = new Spark(PWMPort);
+    }
+
+    public void setLEDColor(double PWMValue) { // the PWM value is from -1 to 1 so it's not really a PWM value, it's normalized
+        blinkin.set(PWMValue);
+    }
+    
+}
