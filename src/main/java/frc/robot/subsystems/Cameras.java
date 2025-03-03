bed80e2aeb26bb23abf10b5da127e61b125ebf9b
Anish Rathod <1anishrathod@gmail.com>
2025-03-02 21:14:31 -0500

    Photonvision makes programming and getting data very easy. TODO: update the reference pose because we are using the "Closest_To_Reference_Pose" strategy which requires updating the reference pose for the most accuracy. We could probably just use this for autos and set the reference pose to wherever pathplanner thinks we are on the field. Not a lot of linear algebra at this point, with just one transform 3D!!

--- src/main/java/frc/robot/subsystems/Cameras.java
+++ src/main/java/frc/robot/subsystems/Cameras.java
@@ -5,9 +5,11 @@ import java.util.Optional;
 import org.photonvision.EstimatedRobotPose;
 import org.photonvision.PhotonCamera;
 import org.photonvision.PhotonPoseEstimator;
+import org.photonvision.PhotonPoseEstimator.PoseStrategy;
 
 import edu.wpi.first.apriltag.AprilTagFieldLayout;
 import edu.wpi.first.apriltag.AprilTagFields;
+import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation3d;
 import edu.wpi.first.math.geometry.Transform3d;
 import edu.wpi.first.math.geometry.Translation3d;
@@ -27,18 +29,32 @@ public class Cameras extends SubsystemBase{
     public static final AprilTagFieldLayout kTagLayout =
         AprilTagFieldLayout.loadField(AprilTagFields.kDefaultField);
 
+    public static AprilTagFieldLayout aprilTagFieldLayout = AprilTagFieldLayout.loadField(AprilTagFields.kDefaultField);
+
+
     public PhotonCamera camera1;
     public PhotonCamera camera2;
     public PhotonCamera camera3;
+
+    PhotonPoseEstimator photonPoseEstimator = new PhotonPoseEstimator(aprilTagFieldLayout, PoseStrategy.CLOSEST_TO_REFERENCE_POSE, kRobotToCam);
 
     public Cameras(String cam1Name, String cam2Name, String cam3Name) {
         camera1 = new PhotonCamera(cam2Name);
         camera2 = new PhotonCamera(cam2Name);
         camera3 = new PhotonCamera(cam3Name);
+    }
 
-        //Transform3d cam1Transform3d = new Transform3d(null, null);
-
-        //photonEstimator = new PhotonPoseEstimator(cam1Transform3d, kTagLayout);
+    public void updateCameraPose() {
+        var result = camera1.getLatestResult();
+        if (result.getBestTarget().getPoseAmbiguity() < 0.2) {
+            Transform3d fieldToCamera = result.getBestTarget().getBestCameraToTarget();
+        }
+    }
+    
+    public Optional<EstimatedRobotPose> getEstimatedGlobalPose(Pose2d prevEstimatedRobotPose) {
+        photonPoseEstimator.setReferencePose(prevEstimatedRobotPose);
+        var result = camera1.getLatestResult();
+        return photonPoseEstimator.update(result);
     }
 
 
