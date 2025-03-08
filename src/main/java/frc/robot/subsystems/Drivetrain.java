2ee184d30fd4c633a6bd8af3afa62039549ab88a
Anish Rathod <1anishrathod@gmail.com>
2025-03-07 20:33:47 -0500

    Now we don't need to make seperate paths for red. The blue autos will just be flipped.

--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -21,6 +21,7 @@ import edu.wpi.first.math.kinematics.SwerveDriveOdometry;
 import edu.wpi.first.math.kinematics.SwerveModulePosition;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
 import edu.wpi.first.util.WPIUtilJNI;
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.Constants.AutoConstants;
@@ -408,7 +409,17 @@ public class Drivetrain extends SubsystemBase {
         AutoConstants.translationConstants, 
         AutoConstants.rotationConstants, 0.02), 
         robotConfig, 
-        () -> false, 
+        () -> {
+              // Boolean supplier that controls when the path will be mirrored for the red alliance
+              // This will flip the path being followed to the red side of the field.
+              // THE ORIGIN WILL REMAIN ON THE BLUE SIDE
+
+              var alliance = DriverStation.getAlliance();
+              if (alliance.isPresent()) {
+                return alliance.get() == DriverStation.Alliance.Red;
+              }
+              return false;
+            }, 
         this);
   }      
   
